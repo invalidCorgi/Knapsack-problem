d5bfb46026b92cbdd6b325f4b6667aa00a6c2562
Wojciech Regulski <wojtekreg@gmail.com>
2017-05-20 23:00:21 +0200

    initial

--- Knapsack_problem/Program.cs
+++ Knapsack_problem/Program.cs
@@ -8,6 +8,14 @@ namespace Knapsack_problem
 {
     class Program
     {
+        int[,] InstantionGenerator(int n) 
+        {
+            var rand = new Random();
+            var intantion = new int[2,n];
+
+            return intantion;
+        }
+
         static void Main(string[] args)
         {
         }
